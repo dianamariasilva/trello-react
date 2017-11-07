6d2518ee31cf7ac64f8ed14d1521b4e668005835
dianamariasilva <diana.silva.v94@gmail.com>
2017-11-07 08:47:15 -0500

    vistas sign up y sign in enlazadas

--- src/SignIn.js
+++ src/SignIn.js
@@ -6,68 +6,65 @@ import './App.css';
 
 const SignIn = () => {
     return(
-<main id="main_container" role="main">
-<div data-reactid=".0">
-<div
-className="view-container registrations new"
-data-reactid=".0.0">
-<main data-reactid=".0.0.0">
-<header data-reactid=".0.0.0.0">
-<div className="logo" data-reactid=".0.0.0.0.0" />
-</header>
-<form id="sign_up_form" data-reactid=".0.0.0.1">
-<div className="field" data-reactid=".0.0.0.1.0">
-<input
-id="user_first_name"
-type="text"
-placeholder="First name"
-required
-data-reactid=".0.0.0.1.0.0" />
-</div>
-<div className="field" data-reactid=".0.0.0.1.1">
-<input
-id="user_last_name"
-type="text"
-placeholder="Last name"
-required
-data-reactid=".0.0.0.1.1.0" />
-</div>
-<div className="field" data-reactid=".0.0.0.1.2">
-<input
-id="user_email"
-type="email"
-placeholder="Email"
-required
-data-reactid=".0.0.0.1.2.0" />
-</div>
-<div className="field" data-reactid=".0.0.0.1.3">
-<input
-id="user_password"
-type="password"
-placeholder="Password"
-required
-data-reactid=".0.0.0.1.3.0" />
-</div>
-<div className="field" data-reactid=".0.0.0.1.4">
-<input
-id="user_password_confirmation"
-type="password"
-placeholder="Confirm password"
-required
-data-reactid=".0.0.0.1.4.0" />
-</div>
-<button type="submit" data-reactid=".0.0.0.1.5">
-Sign up
+<div>
+            <main id="main_container" role="main">
+                <div data-reactid=".0">
+                    <div
+                        className="view-container sessions new"
+                        data-reactid=".0.0">
+                        <main data-reactid=".0.0.0">
+                            <header data-reactid=".0.0.0.0">
+                                <div className="logo" data-reactid=".0.0.0.0.0" />
+                            </header>
+                            <form id="sign_in_form" data-reactid=".0.0.0.1">
+                                <div className="field" data-reactid=".0.0.0.1.1">
+                                    <input
+                                        type="Email"
+                                        id="user_email"
+                                        placeholder="Email"
+                                        required
+                                        defaultValue="john@phoenix-trello.com"
+                                        data-reactid=".0.0.0.1.1.0" />
+                                </div>
+                                <div className="field" data-reactid=".0.0.0.1.2">
+                                    <input
+                                        type="password"
+                                        id="user_password"
+                                        placeholder="Password"
+                                        required
+                                        defaultValue={12345678}
+                                        data-reactid=".0.0.0.1.2.0" />
+                                </div>
+                                <button type="submit" data-reactid=".0.0.0.1.3">
+                                    Sign in
 </button>
-</form>
-<a href="/sign_in" data-reactid=".0.0.0.2">
-Sign in
+                            </form>
+                            <a href="/SignUp" data-reactid=".0.0.0.2">
+                                Create new account
 </a>
-</main>
-</div>
-</div>
-</main>
-
+                        </main>
+                    </div>
+                </div>
+            </main>
+            <footer id="main_footer">
+                <small>
+                    <a href="https://trello.com/" target="_blank">Trello</a> tribute for educational purposes
+            crafted with ♥ for <a href="https://diacode.com/">Diacode</a>
+                    by <a href="https://twitter.com/bigardone">@bigardone</a>
+                </small>
+            </footer>
+            <iframe
+                id="sumome-jquery-iframe"
+                title="Sumo Hidden Content"
+                style={{ display: 'none' }} />
+            <a
+                href="javascript:void(0);"
+                title="Sumo"
+                style={{ backgroundColor: 'rgb(0, 115, 183)', borderRadius: '3px 0px 0px 3px', boxShadow: 'rgba(0, 0, 0, 0.2) 0px 4px 10px', position: 'fixed', zIndex: 2147483647, padding: 0, width: 44, height: 40, textIndent: '-10000px', opacity: 1, display: 'block !important', top: 40, right: '-40px' }}>
+                <span style={{ position: 'absolute', left: '-10000px', top: 'auto', width: 1, height: 1, overflow: 'hidden', marginLeft: 4, marginRight: 0, borderRadius: '3px 0px 0px 3px' }}>Sumo</span>
+                <span style={{ display: 'block', width: 40, height: 40, background: 'url("data:image/png', marginLeft: 4, borderRadius: '3px 0px 0px 3px', marginRight: 0 }} />
+            </a>
+        </div>
 )
 } 
 
